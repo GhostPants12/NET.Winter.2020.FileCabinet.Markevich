474a0e78cb7436c7565e8eb64d76e969c753e6a9
GhostPants12 <54960078+GhostPants12@users.noreply.github.com>
2020-03-18 14:58:41 +0300

    Command refactoring by adding chain of responsibility pattern

---
+++ FileCabinetApp/CommandHandlers/CommandHandlerBase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FileCabinetApp.CommandHandlers
+{
+    public abstract class CommandHandlerBase : ICommandHandler
+    {
+        private ICommandHandler nextHandler;
+
+        public void Handle(AppCommandRequest request)
+        {
+
+        }
+
+        public void SetNext(ICommandHandler handler)
+        {
+            this.nextHandler = handler;
+        }
+    }
+}
